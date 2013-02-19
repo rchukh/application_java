7b84a9826c526d6584024503194c588c92b7759f
Guilhem Lettron <guilhem@lettron.fr>
2013-02-19 15:34:36 +0100

    remove tabulations in provider_java_local_file.rb

--- libraries/provider_java_local_file.rb
+++ libraries/provider_java_local_file.rb
@@ -21,35 +21,35 @@ require 'chef/provider/file'
 
 class Chef
   class Provider
-		class File
-			class JavaLocalFile < Chef::Provider::File
+    class File
+      class JavaLocalFile < Chef::Provider::File
 
-      def load_current_resource
-        @new_resource.path @new_resource.release_path
-        super
+        def load_current_resource
+          @new_resource.path @new_resource.release_path
+          super
+        end
+
+        def action_deploy
+          action_create
+        end
+
+        def action_force_deploy
+          action_create
+        end
+
+        def set_content
+          unless compare_content
+            backup @new_resource.path if ::File.exists?(@new_resource.path)
+            ::FileUtils.cp_r(@new_resource.content, @new_resource.path)
+            Chef::Log.info("#{@new_resource.content} copied to #{@new_resource.path}")
+            @new_resource.updated_by_last_action(true)
+          end
+        end
+
+        def compare_content
+          checksum(@current_resource.path) == checksum(@new_resource.content)
+        end
       end
-
-      def action_deploy
-        action_create
-      end
-
-			def action_force_deploy
-				action_create
-			end
-
-			def set_content
-				unless compare_content
-					backup @new_resource.path if ::File.exists?(@new_resource.path)
-					::FileUtils.cp_r(@new_resource.content, @new_resource.path)
-					Chef::Log.info("#{@new_resource.content} copied to #{@new_resource.path}")
-					@new_resource.updated_by_last_action(true)
-				end
-			end
-
-			def compare_content
-				checksum(@current_resource.path) == checksum(@new_resource.content)
-			end
-			end
     end
   end
 end
