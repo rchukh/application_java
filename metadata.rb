e8833168128f583a0e5ab0cd6ccc3693b75d116b
Roman Chukh <roman.chukh@gmail.com>
2014-05-04 22:54:27 +0300

    Updated application dependency version

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Deploys and configures Java-based applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.0.1"
+version          "3.0.2"
 
 %w{ java tomcat }.each do |cb|
   depends cb
 end
 
-depends "application", "~> 3.0"
+depends "application", "~> 4"
